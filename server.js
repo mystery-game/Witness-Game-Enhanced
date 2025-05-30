37b9757921e393a31b4966821023db37bae571fe
the-batman-627 <henryappel@gmail.com>
2025-05-29 20:08:21 -0400

    Fix: Serve public/index.html and use JS for version banner update time

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the game
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Health check endpoint
